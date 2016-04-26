a4126abc856205fa8c330275b1ac836bc07e75eb
Jimmy Puckett <jimmy.puckett@spinen.com>
2016-04-26 16:13:40 -0400

    mocking out the 2 classes before building out the tests

--- tests/ui/interface_test.py
+++ tests/ui/interface_test.py
@@ -3,26 +3,40 @@ import pytest
 
 # Setup the mock object
 MockAdafruit_CharLCD = Mock()
-# Set the values for the constants on the mock object
-MockAdafruit_CharLCD.SELECT = 'SELECT'
-MockAdafruit_CharLCD.LEFT = 'LEFT'
-MockAdafruit_CharLCD.UP = 'UP'
-MockAdafruit_CharLCD.DOWN = 'DOWN'
-MockAdafruit_CharLCD.RIGHT = 'RIGHT'
+
+
+# # Set the values for the constants on the mock object
+# MockAdafruit_CharLCD.SELECT = 'SELECT'
+# MockAdafruit_CharLCD.LEFT = 'LEFT'
+# MockAdafruit_CharLCD.UP = 'UP'
+# MockAdafruit_CharLCD.DOWN = 'DOWN'
+# MockAdafruit_CharLCD.RIGHT = 'RIGHT'
+
+
+# @pytest.fixture()
+# def mock_lcd():
+#     mock = Mock()
+#
+#     return mock
+
+@pytest.fixture()
+def mock_lcd_plate():
+    return Mock()
 
 
 @pytest.fixture()
-def mock_lcd():
-    mock = Mock()
-
-    return mock
+def mock_navigation():
+    return Mock()
 
 
-# Intercept the import in the LcdPlate since module may not even be available
+@pytest.fixture()
 @patch.dict('sys.modules', Adafruit_CharLCD=MockAdafruit_CharLCD)
-@pytest.fixture()
-def interface(mock_lcd):
+def interface(mock_lcd_plate, mock_navigation):
     # Do import at this place, so that the patch will catch the mocked class
     from coop.ui.interface import UserInterface
 
-    return UserInterface(mock_lcd)
+    return UserInterface(mock_lcd_plate, mock_navigation)
+
+
+def test_first(interface):
+    assert True == True
